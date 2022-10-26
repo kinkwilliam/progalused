17908f2fcb3e16be80177f14f806e2d1df55bb2e
William <william.kink@voco.ee>
2022-10-26 01:42:41 -0700

    Loodud projekti .gitignore fail Ülesanne Hello World Ülesanne: Matemaatilised avaldised läbitud

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
+def absmax (a : int, b: int) -> int:
+Return a max value of two inputs."""""
+
+Absolute values of the inputs are
+used when comparing.
+Returns original (non-absolute)
+value.
+return max ( (a, b), key=abs)
+"""""
+
 print("Hello world!")
+
+
+
